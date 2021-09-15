60847a7b50abba02efe0056c6d2cc6aded603f43
callfoundation <75922734+callfoundation@users.noreply.github.com>
2021-09-15 11:28:33 +0530

    Add files via upload

--- js/calltopnav.js
+++ js/calltopnav.js
@@ -6,3 +6,15 @@ function topnavbar() {
     x.className = "topnav";
   }
 }
+
+function download80g(){
+  var link = document.createElement("a");
+    // If you don't know the name or want to use
+    // the webserver default set name = ''
+    //link.setAttribute('download', ' ');
+    link.href = "js/recipt.docx";
+    //document.body.appendChild(link);
+    link.click();
+    link.remove();
+
+}
